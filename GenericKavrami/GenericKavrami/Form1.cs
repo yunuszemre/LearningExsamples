c4087fba5b2889c1c8f0acec834494dc04793d16
Yunus Emre Teke <yeteke68@gmail.com>
2022-07-24 10:07:41 +0300

    Generic Classes

---
+++ GenericKavrami/GenericKavrami/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenericKavrami
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Generic => Tasarlanan interface, class,method yada parametrelerin belirli bir şablon yapısında çalışması için generic tanımlanabilir
+
+        ArrayList arrayCollection = new ArrayList();
+
+        //Generic
+        List<string> listCollection = new List<string>();
+        List<double> doubleCollection = new List<double>();
+        List<int> intCollection = new List<int>();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    
+}
---
+++ GenericKavrami/GenericKavrami/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenericKavrami
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            GenericMethod(new Button());
+            GenericMethod<Button>(new Button());
+            GenericMethod("asd", 1223);
+        }
+
+        private void GenericMethod<T>(T parameter)
+        {
+
+        }
+        private void GenericMethod(object paramter)
+        {
+
+        }
+
+        private void GenericMethod<T, U>(T param1, U param2)
+        {
+
+        }
+    }
+}
